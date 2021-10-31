fbe47589d0413f6e36f5bf5bb2f4ae8719007033
OutdoorActivity <gorbachyov.fm@gmail.com>
2021-10-31 16:11:12 +0500

    pom.xml & .java files

---
+++ src/main/java/ru/gorbachyov/example/SomeClass2.java
@@ -0,0 +1,10 @@
+package ru.gorbachyov.example;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class SomeClass2 {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase(args[0]));
+    }
+}
